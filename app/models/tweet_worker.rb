170c9139ac4825b6c815088297a4e46d50273a09
Apprentice <apprentice@dbc-chicago-009.local>
2013-10-02 15:28:28 -0500

    add new tweet to page when process is done

--- app/models/tweet_worker.rb
+++ app/models/tweet_worker.rb
@@ -7,9 +7,9 @@ class TweetWorker
     p user.username
 
     tweeter = Twitter::Client.new(oauth_token: user.oauth_token, oauth_token_secret: user.oauth_secret)
-    # tweeter.update(tweet.text)
-    sleep(3)
-    p "This is a 3 second lag"
-    p "the tweet said: #{tweet.text}"
+    tweeter.update(tweet.text)
+    # sleep(3)
+    # p "This is a 3 second lag"
+    # p "the tweet said: #{tweet.text}"
   end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,27 +1,31 @@
 var intervalId;
 var done = "false";
+var tweet_content;
 
 function interval_check (jid) {
-  console.log("interval_check was called")
-  intervalId = setInterval(done_yet, 500,jid);
-};
-
-function done_yet (job_id){
-  console.log("done_yet was called, and job id was "+job_id);
-  $.post('/status/'+job_id, function(data){
-    done = data;  //"true" or "false"
-  });
+  intervalId = setInterval(done_yet, 300, jid);
 };
 
 function stop_checking() {
+  console.log("it's done! we can stop checking!")
   clearInterval(intervalId);
+  $('li:first-child').prepend('<li>' +tweet_content+ '</li>')
 };
-
+ 
+function done_yet (job_id){
+  $.post('/status/'+job_id, function(data){
+    done = data;  //"true" or "false"
+  });
+  console.log("done = "+done);
+  if (done === "true") {
+    stop_checking();
+  }
+};
 
 $(document).ready(function() {
   $('#submit_tweet').submit(function(event){
     event.preventDefault();
-    var tweet_content = $('#input_tweet').val();
+    tweet_content = $('#input_tweet').val();
 
     $.post('/submit_tweet', {tweet: tweet_content}, function(jid) {
       interval_check(jid);
