4cb75060dc2183dc2d474c243c5cb46be70c0c27
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-01 20:00:44 -0500

    store users after they log in w twitter

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -13,13 +13,23 @@ get '/sign_out' do
 end
 
 get '/auth' do
-  # the `request_token` method is defined in `app/helpers/oauth.rb`
   @access_token = request_token.get_access_token(:oauth_verifier => params[:oauth_verifier])
-  # our request token is only valid until we use it to get an access token, so let's delete it from our session
   session.delete(:request_token)
+
+  @user = User.new
+  @user.username = @access_token.params[:screen_name]
+  @user.oauth_token = @access_token.token
+  @user.oauth_secret = @access_token.secret
+  @user.save
 
   # at this point in the code is where you'll need to create your user account and store the access token
 
   erb :index
   
+end
+
+post '/submit_tweet' do 
+  p session
+  # Twitter.update(params[:tweet])
+  # redirect to "/"
 end
